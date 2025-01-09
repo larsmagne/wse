1052f08c4f21a983336f2756b1017932e225c792
Lars Ingebrigtsen <larsi@gnus.org>
2025-01-09 07:56:51 +0100

    Scaffolding

---
+++ bang.el
@@ -0,0 +1,26 @@
+;;; bang.el --- Show Wordpress statistics -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Free Software Foundation, Inc.
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+
+;; bang is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; bang is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl-lib)
+
+
+(provide 'bang)
+
+;;; bang.el ends here
