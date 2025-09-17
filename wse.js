f241f15593dd7d0819677622c0330a1ed2c90fa6
Lars Ingebrigtsen <larsi@gnus.org>
2025-09-17 15:01:36 +0200

    Let ChatGPT rewrite to be jQuery free

--- wse.js
+++ wse.js
@@ -1,144 +1,145 @@
 document.addEventListener(
   "DOMContentLoaded",
   function() {
+    // Helper: POST like jQuery.ajax({ type:'POST', data:{...} })
+    function postWSE(data) {
+      return fetch("/wp-content/plugins/wse/visit.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        body: new URLSearchParams(data).toString(),
+      });
+    }
+
     function instrumentElements() {
       // Instrument all links so that we can see what the user clicked.
-      jQuery("a").each(function() {
-	var link = this.getAttribute("href");
-	if (!link)
+      document.querySelectorAll("a").forEach(function(a) {
+        var link = a.getAttribute("href");
+        if (!link)
 	  return;
-	if (this.getAttribute("data-wse"))
+        if (a.getAttribute("data-wse"))
 	  return;
-	this.setAttribute("data-wse", "true");
-	jQuery(this).click(function(e) {
-	  // Only record clicks with left or middle mouse button.
-	  if (e.which != 1 && e.which != 2)
+        a.setAttribute("data-wse", "true");
+
+        a.addEventListener("click", function(e) {
+          // jQuery uses e.which (1=L, 2=M, 3=R); native uses e.button (0=L,1=M,2=R).
+          var which = e.which ||
+	      (typeof e.button === "number" ? e.button + 1 : 1);
+
+          // Only record clicks with left or middle mouse button.
+          if (which !== 1 && which !== 2)
 	    return true;
-	  var go = function(result) {
-	    // Middle mouse button opens new tab/window.
-	    if (e.which == 1)
+
+          var go = function() {
+            // Middle mouse button opens new tab/window.
+            if (which === 1)
 	      window.location.href = link;
-	    else
+            else
 	      window.open(link, "_blank");
-	  };
-	  jQuery.ajax({
-	    url: "/wp-content/plugins/wse/visit.php",
-	    type: "POST",
-	    data: {
-	      "click": link,
-	      "page": window.location.href
-	    },
-	    dataType: "json",
-	    success: go,
-	    error: go
-	  });
-	  e.preventDefault();
-	  return true;
-	});
+          };
+
+          postWSE({
+            click: link,
+            page: window.location.href,
+          })
+            .then(go)
+            .catch(go);
+
+          e.preventDefault();
+          return true;
+        });
       });
 
       // Instrument <video> elements.
-      jQuery("video").each(function() {
-	var link;
-	var $source = jQuery(this).find("source");
-	if (!this.autoplay && $source.length == 1) {
-	  if (this.getAttribute("data-wse"))
+      document.querySelectorAll("video").forEach(function(video) {
+        var sources = video.querySelectorAll("source");
+        if (!video.autoplay && sources.length === 1) {
+          if (video.getAttribute("data-wse"))
 	    return;
-	  this.setAttribute("data-wse", "true");
-	  link = $source.attr("src");
-	  jQuery(this).on("playing", function() {
-	    jQuery(this).off("playing");
-	    var link;
-	    var $source = jQuery(this).find("source");
-	    if ($source.length == 1) {
-	      link = $source.attr("src");
-	      jQuery.ajax({
-		url: "/wp-content/plugins/wse/visit.php",
-		type: "POST",
-		data: {
-		  "click": link,
-		  "page": window.location.href
-		},
-		dataType: "json",
-		success: function(result) {
-		}
-	      });
-	    }
-	    return true;
-	  });
-	}
+          video.setAttribute("data-wse", "true");
+
+          var onPlaying = function() {
+            video.removeEventListener("playing", onPlaying);
+            var s2 = video.querySelectorAll("source");
+            if (s2.length === 1) {
+              var link = s2[0].getAttribute("src");
+              if (link) {
+                postWSE({
+                  click: link,
+                  page: window.location.href,
+                });
+              }
+            }
+            return true;
+          };
+
+          video.addEventListener("playing", onPlaying);
+        }
       });
 
       // Instrument Lyte video elements.
-      jQuery(".lyte-wrapper > div[itemprop='video']").click(function() {
-	if (this.id) {
-	  if (this.getAttribute("data-wse"))
-	    return;
-	  this.setAttribute("data-wse", "true");
-	  var link = "https://www.youtube.com/watch?v=" +
-	      this.id.replace(/^WYL_/, "");
-	  jQuery.ajax({
-	    url: "/wp-content/plugins/wse/visit.php",
-	    type: "POST",
-	    data: {
-	      "click": link,
-	      "page": window.location.href
-	    },
-	    dataType: "json",
-	    success: function(result) {
-	    }
-	  });
-	}
-      });
-    };
-
-    instrumentElements();
+      document
+        .querySelectorAll(".lyte-wrapper > div[itemprop='video']")
+        .forEach(function(el) {
+          el.addEventListener("click", function() {
+            if (this.id) {
+              if (this.getAttribute("data-wse"))
+		return;
+              this.setAttribute("data-wse", "true");
+              var link =
+                  "https://www.youtube.com/watch?v=" +
+                  this.id.replace(/^WYL_/, "");
+              postWSE({
+                click: link,
+                page: window.location.href,
+              });
+            }
+          });
+        });
+    }
 
     var registerVisit = function(referrer) {
       // Get the title from the entry title, but fall back on the
       // document title.
       var title;
-      var $title = jQuery(".entry-title");
+      var titleEls = document.querySelectorAll(".entry-title");
       // If we have more than one hit, we're probably on the front page,
       // so fall back on the document title instead.
-      if ($title.length == 1)
-	title = $title.text();
+      if (titleEls.length === 1)
+	title = titleEls[0].textContent;
       if (!title) {
-	title = document.title;
-	if (title)
+        title = document.title;
+        if (title)
 	  title = title.replace(/ +\u2013.*$/, "");
       }
-      jQuery.ajax({
-	url: "/wp-content/plugins/wse/visit.php",
-	type: "POST",
-	data: {
-	  "ref": referrer,
-	  "page": window.location.href,
-	  "title": title
-	},
-	dataType: "json",
-	success: function(result) {
-	}
+
+      postWSE({
+        ref: referrer,
+        page: window.location.href,
+        title: title || "",
       });
     };
 
-    // Detect when the document href changes.  This happens when using
+    instrumentElements();
+
+    // Detect when the document href changes. This happens when using
     // "infinite scroll", particularly on the front pages.
     var oldHref = document.location.href;
     setInterval(function() {
       if (oldHref != document.location.href) {
-	registerVisit(oldHref);
+        registerVisit(oldHref);
         oldHref = document.location.href;
       }
     }, 1000);
 
     // Check the document to see if we need to instrument new elements
     // that may have arrived via infinite scrolling.
-    var observer = new MutationObserver(function(mutations) {
+    var observer = new MutationObserver(function() {
       instrumentElements();
     });
-    
-    observer.observe(document.querySelector('body'), {
+
+    observer.observe(document.querySelector("body"), {
       childList: true,
       subtree: true
     });
@@ -146,4 +147,5 @@ document.addEventListener(
     // Record that we've loaded this page.
     registerVisit(document.referrer);
   },
-  false);
+  false
+);
