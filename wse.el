b5b23f6d60e42033a2f98ffb8d16a8dd4284d142
Lars Ingebrigtsen <larsi@gnus.org>
2025-10-28 06:47:26 +0100

    Fix up ?replytocom handling

--- wse.el
+++ wse.el
@@ -849,7 +849,7 @@ I.e., \"google.com\" or \"google.co.uk\"."
 	(replace-regexp-in-string
 	 "/page/[0-9]+/\\'" "/"
 	 (replace-regexp-in-string
-	  "\\?fbclid.*\\|\\?from=.*utm_.*\\|\\?utm_.*\\|\\?theme.*\\|\\?replytocom"
+	  "\\?fbclid.*\\|\\?from=.*utm_.*\\|\\?utm_.*\\|\\?theme.*\\|\\?replytocom.*"
 	  ""
 	  url)))))
 
