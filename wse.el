5b38563736cb5ccabfe5db1717c45196358cbc13
Lars Ingebrigtsen <larsi@gnus.org>
2026-02-04 20:32:22 +0100

    Strip _bhlid parameters from URLs

--- wse.el
+++ wse.el
@@ -868,7 +868,7 @@ I.e., \"google.com\" or \"google.co.uk\"."
 	(replace-regexp-in-string
 	 "/page/[0-9]+/\\'" "/"
 	 (replace-regexp-in-string
-	  "\\?fbclid.*\\|\\?from=.*utm_.*\\|\\?utm_.*\\|\\?theme.*\\|\\?replytocom.*"
+	  "\\?fbclid.*\\|\\?from=.*utm_.*\\|\\?utm_.*\\|\\?theme.*\\|\\?replytocom.*\\|\\?_bhlid.*"
 	  ""
 	  url)))))
 
